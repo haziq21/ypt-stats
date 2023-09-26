74f37abcd704e86384da9f1280c2d32728117720
Haziq Hairil <haziqhairil21@gmail.com>
2023-09-26 21:05:52 +0800

    Prototype GUI flow

--- main.tsx
+++ main.tsx
@@ -1,15 +1,22 @@
 /** @jsx jsx */
-import { Hono } from "https://deno.land/x/hono@v3.7.0-rc.1/mod.ts";
 import { jsx } from "https://deno.land/x/hono@v3.7.0-rc.1/middleware.ts";
+import { Hono } from "npm:hono";
 import {
+  deleteCookie,
   getSignedCookie,
   setSignedCookie,
-} from "https://deno.land/x/hono@v3.7.1/helper.ts";
+} from "npm:hono/cookie";
 import { z } from "npm:zod";
-import { Resvg } from "npm:@resvg/resvg-js";
-import { qrcode } from "https://deno.land/x/qrcode@v2.0.0/mod.ts";
-import "https://deno.land/x/dotenv@v3.2.2/load.ts";
-import { createOneTimeGroup, waitForMember } from "./ypt.ts";
+
+import "https://deno.land/std@0.202.0/dotenv/load.ts";
+// import { Resvg } from "npm:@resvg/resvg-js";
+// import { qrcode } from "https://deno.land/x/qrcode@v2.0.0/mod.ts";
+import {
+  createOneTimeGroup,
+  getStudyStats,
+  Stats,
+  waitForMember,
+} from "./ypt.ts";
 
 const userSchema = z.object({
   id: z.number(),
@@ -20,36 +27,95 @@ const PRIVATE_KEY = Deno.env.get("PRIVATE_KEY")!;
 
 const app = new Hono();
 
+const SummaryStats = (props: Stats) => (
+  <div>
+    <p>
+      {Math.round(props.totalStudyTime / 1000 / 60 / 60)}h total study time!!!
+    </p>
+    <p>i havent formatted these: {JSON.stringify(props)}</p>
+  </div>
+);
+
+const JoinGroup = (props: {
+  name: string;
+  password: string;
+  link: string;
+}) => (
+  <div hx-get="/stats-loader" hx-trigger="load">
+    <p>
+      join this group rn <a href={props.link}>{props.name}</a>
+    </p>
+    (password is {props.password})
+  </div>
+);
+
+const StatsLoader = (props: { name: string }) => (
+  <div hx-post="/stats" hx-trigger="load" hx-target="#stats">
+    <h1>hi {props.name}</h1>
+    <p>im loading your stats (alr deleted the grp)</p>
+    <div id="stats"></div>
+  </div>
+);
+
 // Homepage
-app.get("/", (c) => c.html(<h1>hi!!</h1>));
+app.get("/", (c) => {
+  // TODO: check if the "user" cookie is set
 
-app.get("/otg", async (c) => {
-  // Create a one-time YPT group to identify and authenticate the user
-  const group = await createOneTimeGroup();
-
-  await setSignedCookie(c, "otg", group.id.toString(), PRIVATE_KEY);
-  return c.json(group);
+  return c.html(
+    <html>
+      <head>
+        <title>YPT stats generator</title>
+        <script src="https://unpkg.com/htmx.org@1.9.6"></script>
+      </head>
+      <body hx-target="body">
+        <p>hi this is the ypt stats generator!!</p>
+        <button hx-post="/otg">lets GOo</button>
+      </body>
+    </html>,
+  );
 });
 
-app.get("/stats", async (c) => {
-  const groupId = await getSignedCookie(c, PRIVATE_KEY, "otg");
+app.post("/otg", async (c) => {
+  // Create a one-time YPT group to identify and authenticate the user
+  const group = await createOneTimeGroup();
+  // So we can identify the user on the /stats endpoint
+  await setSignedCookie(c, "otg", group.id.toString(), PRIVATE_KEY);
 
+  return c.html(<JoinGroup {...group} />);
+});
+
+app.get("/stats-loader", async (c) => {
+  const groupId = await getSignedCookie(c, PRIVATE_KEY, "otg");
   if (!groupId) return c.body(null, 400);
 
   const user = await waitForMember(Number.parseInt(groupId));
+
+  deleteCookie(c, "otg");
   await setSignedCookie(c, "user", JSON.stringify(user), PRIVATE_KEY);
 
-  return c.json(user);
+  return c.html(<StatsLoader name={user.name} />);
 });
 
-app.get("/image", async (c) => {
-  const userCookie = await getSignedCookie(c, PRIVATE_KEY, "user");
+app.post("/stats", async (c) => {
+  const serializedUser = await getSignedCookie(c, PRIVATE_KEY, "user");
+  if (!serializedUser) return c.body(null, 400);
 
+  const user = userSchema.parse(JSON.parse(serializedUser));
+
+  // Generate the stats
+  const stats = await getStudyStats(user.id);
+
+  return c.html(<SummaryStats {...stats} />);
+});
+
+app.post("/image", async (c) => {
+  const userCookie = await getSignedCookie(c, PRIVATE_KEY, "user");
   if (!userCookie) return c.body(null, 400);
 
   const user = userSchema.parse(JSON.parse(userCookie));
+  const studyStats = await getStudyStats(user.id);
 
-  return c.json(user);
+  return c.json({ ...user, ...studyStats });
 });
 
 Deno.serve({ port: 3000 }, app.fetch);
