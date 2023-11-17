7976fc788dfe5cac107017a671754228162eeebe
Haziq Hairil <haziqhairil21@gmail.com>
2023-11-17 13:46:17 +0800

    Add <meta> viewport tag

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -3,14 +3,15 @@ import { tw } from "npm:twind";
 export default (props: { style: string }) => (
   <html>
     <head>
-      <title>YPT stats generator</title>
+      <title>YPT summary statistics</title>
+      <meta name="viewport" content="width=device-width" />
       <script src="https://unpkg.com/htmx.org@1.9.6"></script>
     </head>
     <body class={tw`bg-warmGray-900 text-warmGray-400`}>
       {/* Injected CSS goes in the <div> */}
       <div dangerouslySetInnerHTML={{ __html: props.style }} />
 
-      <nav class={tw`py-5 px-8 text-xl font-bold flex-none`}>
+      <nav class={tw`py-5 px-8 text-xl font-bold`}>
         <span class={tw`text-warmGray-50`}>YPT&nbsp;</span>
         <span class={tw`text-emerald-400`}>Stats</span>
       </nav>
