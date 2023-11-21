620907b6d0c466a7d7130d7f53e8c5df79a416e5
Haziq Hairil <haziqhairil21@gmail.com>
2023-11-21 12:01:07 +0800

    Prototype card template

--- src/components/StatsLoader.tsx
+++ src/components/StatsLoader.tsx
@@ -1,9 +1,43 @@
+import { tw } from "npm:twind";
+
 export default (props: { name: string }) => (
   <>
-    <h1 hx-post="/stats" hx-trigger="load" hx-target="#stats">
-      hi {props.name}
-    </h1>
-    <p>im loading your stats (alr deleted the grp)</p>
-    <div id="stats"></div>
+    <figure
+      class={tw`border(2 warmGray-700) rounded-lg pt-6 pb-3 w-full max-w-xl`}
+    >
+      <div class={tw`flex justify-between items-center mb-8 px-8`}>
+        <div>
+          <h2 class={tw`font-semibold text(4xl warmGray-50) mb-1`}>
+            {props.name}
+          </h2>
+          <p>
+            on<span class={tw`mr-2`} />
+            <span class={tw`text-warmGray-200 font-medium inline-block`}>
+              <img
+                src="/assets/ypt-icon.png"
+                alt=""
+                class={tw`inline-block w-4 -mt-1 mr-1`}
+              />
+              YPT
+            </span>
+          </p>
+        </div>
+
+        <div class={tw`text-right`}>
+          <p
+            class={tw`uppercase tracking-wider font-bold text(xl warmGray-600)`}
+          >
+            Past month
+          </p>
+          <p id="record-streak"></p>
+        </div>
+      </div>
+
+      <div hx-post="/stats" hx-trigger="load" hx-target="this"></div>
+
+      <p class={tw`text(xs warmGray-600) pl-4`}>
+        Generated at https://ypt-stats.deno.dev
+      </p>
+    </figure>
   </>
 );
--- src/components/SummaryStats.tsx
+++ src/components/SummaryStats.tsx
@@ -1,10 +1,14 @@
+import { tw } from "npm:twind";
 import { Stats } from "../ypt.ts";
 
 export default (props: Stats) => (
   <>
-    <p>
+    <p id="record-streak" hx-swap-oob class={tw`text-sm`}>
+      {props.longestStreak} day record streak
+    </p>
+    <p class={tw`px-8 mb-8`}>
       {Math.round(props.totalStudyTime / 1000 / 60 / 60)}h total study time!!!
     </p>
-    <p>i havent formatted these: {JSON.stringify(props)}</p>
+    {/* <p>i havent formatted these: {JSON.stringify(props)}</p> */}
   </>
 );
